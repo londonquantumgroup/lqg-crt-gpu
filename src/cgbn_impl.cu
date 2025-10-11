e0d59cd8dc38497ad47affacd3a8d516affeac8b
sebinkooooo <biz.sebkot@gmail.com>
2025-10-11 18:18:21 +0100

    fix

--- src/cgbn_impl.cu
+++ src/cgbn_impl.cu
@@ -1,6 +1,10 @@
 // src/cgbn_impl.cu
+// This is the ONLY file that should have the full CGBN implementation
+// All other .cu files should have CGBN_NO_IMPLEMENTATION defined
+
 #include <gmp.h>
 #include <gmpxx.h>
 #define CGBN_USE_GMP 1
-#include "cgbn/cgbn.h"   // no CGBN_NO_IMPLEMENTATION here
-// (your cgbn kernels / helpers live here)
+
+// DO NOT define CGBN_NO_IMPLEMENTATION here - this file needs the implementation
+#include <cgbn/cgbn.h>
