c22e53f5e7d608a7cc5d7f26581235a49ab291de
sebinkooooo <biz.sebkot@gmail.com>
2025-10-11 18:21:26 +0100

    fixes

--- include/cgbn_utils.hpp
+++ include/cgbn_utils.hpp
@@ -52,46 +52,4 @@ static inline void host_cgbn_error_report_init(cgbn_error_report_t *report) {
     }
 }
 
-// Now include the full CGBN definitions for files that need them
-// Each .cu file must include cgbn.h separately with proper defines
-
 #endif // NO_CGBN
-
-// Use __host__ __device__ to allow use in both CPU and GPU code
-__host__ __device__ static inline void cppint_to_cgbn_mem_impl(const cpp_int &x, cgbn_bn_mem_t &out) {
-    for (size_t i = 0; i < sizeof(out._limbs) / sizeof(out._limbs[0]); ++i) {
-        out._limbs[i] = 0u;
-    }
-    cpp_int t = x;
-    size_t i = 0;
-    while (t > 0 && i < sizeof(out._limbs) / sizeof(out._limbs[0])) {
-        uint32_t limb = (uint32_t)(t & cpp_int(0xFFFFFFFFu));
-        out._limbs[i] = limb;
-        t >>= 32;
-        ++i;
-    }
-}
-
-// Wrapper to avoid multiple definitions
-static inline void cppint_to_cgbn_mem(const cpp_int &x, cgbn_bn_mem_t &out) {
-    cppint_to_cgbn_mem_impl(x, out);
-}
-
-__host__ __device__ static inline cpp_int cgbn_mem_to_cppint_impl(const cgbn_bn_mem_t &x) {
-    cpp_int result = 0;
-    for (int i = (sizeof(x._limbs) / sizeof(x._limbs[0])) - 1; i >= 0; --i) {
-        result = (result << 32) | cpp_int(x._limbs[i]);
-    }
-    return result;
-}
-
-static inline cpp_int cgbn_mem_to_cppint(const cgbn_bn_mem_t &x) {
-    return cgbn_mem_to_cppint_impl(x);
-}
-
-static inline void host_cgbn_error_report_init(cgbn_error_report_t *report) {
-    if (report) {
-        memset(report, 0, sizeof(cgbn_error_report_t));
-    }
-}
-#endif
