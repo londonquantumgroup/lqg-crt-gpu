4f0c5466d6fe0647607ad419a5e548859f49176d
sebinkooooo <biz.sebkot@gmail.com>
2025-10-11 14:30:22 +0100

    fixes

--- include/crt_utils.hpp
+++ include/crt_utils.hpp
@@ -45,13 +45,13 @@ inline cpp_int read_big_decimal(const std::string &s) {
 }
 
 // CUDA check
-#include <cuda_runtime.h>
-#define CUDA_CHECK(stmt)                                                        \
-  do {                                                                          \
-    cudaError_t err = (stmt);                                                   \
-    if (err != cudaSuccess) {                                                   \
-      fprintf(stderr, "CUDA error %s at %s:%d -> %s\n", #stmt, __FILE__,        \
-              __LINE__, cudaGetErrorString(err));                               \
-      exit(1);                                                                  \
-    }                                                                           \
-  } while (0)
+#ifdef __CUDACC__
+#endif
+#define CUDA_CHECK(stmt) do { \
+    cudaError_t err = (stmt); \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA error %s at %s:%d -> %s\n", \
+                #stmt, __FILE__, __LINE__, cudaGetErrorString(err)); \
+        exit(1); \
+    } \
+} while(0)
